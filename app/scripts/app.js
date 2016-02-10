bd706e880930438b56dd5763b74ff5dba1ed7a0f
Karina Wolff <karinaschmidt@hotmail.com>
2016-02-10 14:51:06 -0800

    deploy yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name capstoneApp
+ * @description
+ * # capstoneApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('capstoneApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

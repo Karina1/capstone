63aafb9f1c38120c49904c9c61b88025901f6ddd
Karina Wolff <karinaschmidt@hotmail.com>
2016-03-11 10:55:00 -0800

    fixed main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,18 @@
 'use strict';
-                  ​
-// /*
-//  * @ngdoc function
-//  * @name capstoneApp.controller:AboutCtrl
-//  * @description
-//  * # MainCtrl
-//  * Controller of the capstoneApp
-//  */
-                angular.module('capstoneApp')
-                   .controller('MainCtrl', function($scope,flickr) {
-                       $scope.flickrData = flickr.query();
-                       $scope.flickrData.$promise.then(function(data){
-                         for(i=0;i<=data.photos.photo.length; i++){
-                           //set up map markers
-                         }
-                       });
-                   });
+/**
+* @ngdoc function
+* @name capstoneApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the capstoneApp
+*/
+
+angular.module('capstoneApp')
+  .controller('MainCtrl', function($scope,flickr){
+    $scope.flickrData = flickr.query();
+    $scope.flickrData.$promise.then(function(data){
+      for(i=0;i<=data.photos.photo.length; i++){
+        //set up map markers
+      }
+    });
+  });
